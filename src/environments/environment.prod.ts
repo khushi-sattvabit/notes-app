6e9ad9c34bce2d145aaab35c7a164272c47964ca
khushi-sattvabit <khushi.patel@sattvabit.com>
2025-04-18 11:40:58 +0530

    chore: Update Firebase configuration in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDYmzW4pOK8iXweBvuXoussc8FXclUhfFM',
+    authDomain: 'notes-app-fc985.firebaseapp.com',
+    projectId: 'notes-app-fc985',
+    storageBucket: 'notes-app-fc985.firebasestorage.app',
+    messagingSenderId: '627038235963',
+    appId: '1:627038235963:web:7270325ecada25a8e99b1e',
+    measurementId: 'G-ESTQW74BQ4',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDYmzW4pOK8iXweBvuXoussc8FXclUhfFM',
+    authDomain: 'notes-app-fc985.firebaseapp.com',
+    projectId: 'notes-app-fc985',
+    storageBucket: 'notes-app-fc985.firebasestorage.app',
+    messagingSenderId: '627038235963',
+    appId: '1:627038235963:web:7270325ecada25a8e99b1e',
+    measurementId: 'G-ESTQW74BQ4',
+  },
 };
 
 /*
