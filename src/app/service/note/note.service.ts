730f551cd2cba03c3c68039e3c1a0ce304ed2cee
khushi-sattvabit <khushi.patel@sattvabit.com>
2025-04-18 11:40:51 +0530

    feat: Add Firestore integration to NoteService for CRUD operations

--- src/app/service/note/note.service.ts
+++ src/app/service/note/note.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@angular/core';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  Firestore,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { BehaviorSubject } from 'rxjs';
 
 export interface Note {
@@ -17,14 +27,15 @@ export class NoteService {
     return this._notes.asObservable();
   }
 
-  constructor() {}
+  constructor(private firestore: Firestore) {}
 
   async addNote(data: Note) {
     try {
-      let currentNotes = this._notes.value;
-      let notes: Note[] = [
-        { ...data, id: (currentNotes.length + 1).toString() },
-      ];
+      let dataRef: any = collection(this.firestore, 'notes');
+      const response = await addDoc(dataRef, data);
+      const id = await response.id;
+      const currentNotes = this._notes.value;
+      let notes: Note[] = [{ ...data, id }];
       notes = notes.concat(currentNotes);
       this._notes.next(notes);
       return notes;
@@ -35,7 +46,32 @@ export class NoteService {
 
   async getNotes() {
     try {
-      return [];
+      let dataRef: any = collection(this.firestore, 'notes');
+      const querySnapshot = await getDocs(dataRef);
+      const notes: Note[] = await querySnapshot.docs.map((doc) => {
+        let item: any = doc.data();
+        item.id = doc.id;
+        return item;
+      });
+      this._notes.next(notes);
+      return notes;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getNotesById(id: string) {
+    try {
+      let dataRef: any = doc(this.firestore, `notes/${id}`);
+      const docSnap = await getDoc(dataRef);
+      if (docSnap.exists()) {
+        let item: any = docSnap.data();
+        item.id = docSnap.id;
+        return { ...item } as Note;
+      } else {
+        console.log('No such document!');
+        throw 'No such document!';
+      }
     } catch (e) {
       throw e;
     }
@@ -43,10 +79,14 @@ export class NoteService {
 
   async updateNote(id: string, data: Note) {
     try {
+      let dataRef: any = doc(this.firestore, `notes/${id}`);
+      await updateDoc(dataRef, data);
       let currentNotes = this._notes.value;
-      let index = currentNotes.findIndex((x) => x.id === id);
-      currentNotes[index] = { id, ...data };
+      const index = currentNotes.findIndex((x) => x.id === id);
+      const latestData = { id, ...data };
+      currentNotes[index] = latestData;
       this._notes.next(currentNotes);
+      return latestData;
     } catch (e) {
       throw e;
     }
@@ -54,6 +94,8 @@ export class NoteService {
 
   async deleteNote(id: string) {
     try {
+      let dataRef: any = doc(this.firestore, `notes/${id}`);
+      await deleteDoc(dataRef);
       let currentNotes = this._notes.value;
       currentNotes = currentNotes.filter((x) => x.id != id);
       this._notes.next(currentNotes);
